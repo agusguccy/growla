e58ecbfdaef1407baf0df3f0d2d32822b2322222
agusguccy <50383854+agusguccy@users.noreply.github.com>
2019-08-29 18:53:42 -0300

    Actualización de DB

--- growla_db.sql
+++ growla_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-08-2019 a las 20:49:42
+-- Tiempo de generación: 29-08-2019 a las 23:58:35
 -- Versión del servidor: 10.1.39-MariaDB
 -- Versión de PHP: 7.3.5
 
@@ -45,9 +45,11 @@ CREATE TABLE `beers` (
 --
 
 INSERT INTO `beers` (`id`, `type`, `description`, `IBUs`, `alcohol_content`, `color_id`, `image`, `created_at`, `updated_at`) VALUES
-(1, 'Blonde Ale', 'blah blah', '2', '4.00', NULL, 'blonde2.jpg', '2019-08-23 02:51:11', '2019-08-23 02:51:11'),
-(2, 'Blonde Ale', 'blah blah', '2', '4.00', NULL, 'blonde2.jpg', '2019-08-23 02:51:52', '2019-08-23 02:51:52'),
-(3, 'Blonde Ale', 'a', '1', '1.00', NULL, 'C:\\xampp\\tmp\\php5D44.tmp', '2019-08-23 03:22:46', '2019-08-23 03:22:46');
+(9, 'Blonde Ale', 'Cerveza dorada de fermentación alta, perteneciente a la familia de las ALE, de moderada intensidad que tiene una complejidad sutil frutal-especiada a levadura belga, un poco de sabor a malta dulce y un final seco.', '28', '4.00', 4, 'uploads/3ScvZygGzcHrjROSoyFYxr9tVxQN59dqzDr8mWMN.jpeg', '2019-08-29 22:05:48', '2019-08-29 22:05:48'),
+(10, 'IPA', 'Cerveza de fermentación alta y calificada como pálida, para diferenciarla de las características sobre las Brown, Stout o Porters. Presentan un color pálido anaranjado similar al ámbar de menor o mayor intensidad y poseen un mayor aporte de lúpulo que proporciona un mayor grado de amargor, aparte de una serie de matices aromáticos y de sabor particular.', '40', '5.00', 3, 'uploads/SsQy31ro69o2SGFgzJxzqBYtcx8stBMM2Lbj2aTA.jpeg', '2019-08-29 22:06:30', '2019-08-29 22:06:30'),
+(11, 'Session IPA', 'Cerveza fácil de beber pero sin dejar de lado las características de la IPA, tiene un sabor algo más seco que las Pale Ales, aunque es innegable la gran similitud entre ambas.  Posee un cuerpo sumamente ligero y junto al lúpulo viene el intenso aroma, pero sin el amargor pronunciado ni el alto porcentaje de alcohol.', '50', '5.00', 3, 'uploads/EPORmFBAQ8xeeyEbbPWWBcvkNfgSopHBbvUdQYq0.jpeg', '2019-08-29 22:09:41', '2019-08-29 22:09:41'),
+(12, 'Okctoberfest', 'El sabor a malta inicial a menudo sugiere dulzura, pero de acabado moderadamente seco. El complejo y único carácter maltoso a menudo incluye notas a pan y tostado. El amargor procedente del lúpulo es moderado, a pesar de que su sabor sea de bajo a imperceptible. Sin embargo, otorgan a la cerveza el equilibrio suficiente para que no predomine el dulzor de la malta.', '30', '6.00', 3, 'uploads/vNzizggbOYX3WjfhdevX0eaLhEzFguTvkP08KrmK.jpeg', '2019-08-29 22:11:13', '2019-08-29 22:11:13'),
+(13, 'Porter', 'Cerveza oscura perteneciente a la familia de las ALE, de moderado bajo aroma de malta tostada, de pan y bizcocho, con un suave olor torrefacto y un tono a chocolate.', '35', '5.00', 2, 'uploads/t7qIsrnV5tBgyDRl4AgN91IQyxW1TGIAmoC5lNee.jpeg', '2019-08-29 22:11:42', '2019-08-29 22:11:42');
 
 -- --------------------------------------------------------
 
@@ -91,7 +93,8 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_16_005940_create_all_tables', 1);
+(3, '2019_08_16_005940_create_all_tables', 1),
+(4, '2019_08_28_002125_add_is_admin_column_to_users_table', 2);
 
 -- --------------------------------------------------------
 
@@ -119,12 +122,25 @@ CREATE TABLE `users` (
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `country` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `city` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   `beer_id` bigint(20) UNSIGNED DEFAULT NULL,
-  `avatar` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL
+  `avatar` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `is_admin` smallint(6) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `surname`, `email`, `email_verified_at`, `password`, `country`, `city`, `remember_token`, `created_at`, `updated_at`, `beer_id`, `avatar`, `is_admin`) VALUES
+(3, 'Agustina', 'Dorado', 'aguswtf@hotmail.com', NULL, '$2y$10$NQg.oKFHUHwN5esfi058u.bY5BPq4oKIS2C5Ec.7Nzx5V8gkBD.cC', 'AR', NULL, NULL, '2019-08-28 02:05:49', '2019-08-28 02:05:49', NULL, 'img_5d65b74d42d6b.jpeg', 0),
+(4, 'Agustina', 'Dorado', 'aguswhat@hotmail.com', NULL, '$2y$10$ZcbfbJ1yb4DEvzHVpkLzEO6B8KrTNjg9Iz21VUumRq5REPekhtJFi', 'AR', NULL, NULL, '2019-08-28 02:25:34', '2019-08-28 02:25:34', NULL, 'img_5d65bbeea7708.jpeg', 0),
+(5, 'Juanito', 'Pepe', 'juanito@dh.com', NULL, '$2y$10$eTYBQFRKfgc.xtOd0uiu9ev/9IBi8KUDunZm/JtaZqWCecdj473Ca', 'ar', NULL, NULL, '2019-08-29 16:56:17', '2019-08-29 16:56:17', NULL, 'img_5d67d9813ca64.jpeg', 0),
+(6, 'Juancito', 'Perez', 'juancito@lacaja.com.ar', NULL, '$2y$10$1IPx1Pg7jHgRJX2Cl4nen.gf4BTr3.OaloiVsrlBwoFgVr6qJHaF6', 'AR', 'Capital Federal', NULL, '2019-08-29 20:40:47', '2019-08-29 20:40:47', NULL, 'img_5d680e1f644ca.jpeg', 0),
+(7, 'Agustina', 'Dorado', 'agus.sr@hotmail.com', NULL, '$2y$10$vOqU4b.bd5unsUEWN2oiRO29gIrvDd7VfmQWHVhj7cEyodqgZ5FyW', 'AR', 'Capital Federal', NULL, '2019-08-29 22:01:36', '2019-08-29 22:01:36', NULL, 'img_5d6821105f47a.jpeg', 1);
 
 --
 -- Índices para tablas volcadas
@@ -171,7 +187,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `beers`
 --
 ALTER TABLE `beers`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `colors`
@@ -183,13 +199,13 @@ ALTER TABLE `colors`
 -- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restricciones para tablas volcadas
